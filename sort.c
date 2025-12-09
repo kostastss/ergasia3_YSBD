e86a5803e13b59fa313be4aabd92dad97b62f4f6
kostastss <146731047+kostastss@users.noreply.github.com>
2025-12-09 02:06:46 +0200

    Update sort.c

--- sort.c
+++ sort.c
@@ -10,21 +10,30 @@
 
 // Επιστρέφει εάν πρέπει να γίνει swap του rec1 με το rec2, συγκρίνοντας αλβαβιτικά το όνομα με το επώνυμο.
 bool shouldSwap(Record* rec1,Record* rec2){
-    if(rec1->name > rec2->name){
+    int cmpr = strcmp(rec1->name, rec2->name);
+    if(cmpr > 0){
         return true;
-    }else if(rec1->name < rec2->name){
+    }else if(cmpr < 0){
         return false;
-    }else{  // Αν τα ονόματα είναι ίδια τότε ελέγχουμε τα επώνυμα.
-        if(rec1->surname > rec2->surname){
-            return true;
-        }else{  // Αν τα επώνυμα είναι ίσα ή "rec1->surname < rec2->surname", τότε false.
-            return false;
-        }
+    }
+
+    // Αν τα ονόματα είναι ίδια τότε ελέγχουμε τα επώνυμα.
+    cmpr = strcmp(rec1->surname, rec2->surname);
+    if(cmpr > 0){
+        return true;
+    }else{  //  Αν τα επώνυμα είναι ίσα ή "rec1->surname < rec2->surname", τότε false.
+        return false;
     }
 }
 
+// Παίρνει CHUNKS από το αρχείο, και τα ταξινομεί ξεχωριστά σε κάθε πέρασμα.
 void sort_FileInChunks(int file_desc, int numBlocksInChunk){
-
+    // Θέλουμε έναν iterator για να ξέρουμε κάθε φορά πιo CHUNK δουλεύουμε. 
+    CHUNK_Iterator it = CHUNK_CreateIterator(file_desc, numBlocksInChunk);
+    CHUNK ch;   // Το CHUNK που θα δουλέψουμε.
+    while(CHUNK_GetNext(&it, &ch) == 0){    // Η GetNext επιστρέφει 0 αν είναι επιτυχής, δηλαδή υπάρχει επόμενο CHUNK.
+        sort_Chunk(&ch);    // Περνάμε στην short το κάθε CHUNK για ταξινόμηση.
+    }
 }
 
 void sort_Chunk(CHUNK* chunk){
